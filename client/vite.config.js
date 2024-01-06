976298e025372ec7b9cbe26c5aae524ce8c82d46
AniketPithadia <aniketpithadia@gmail.com>
2024-01-05 20:39:21 -0500

    files updated

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  proxy: {
+    "/": {
+      target: "https://simple-chat-app-8qet.vercel.app/",
+      changeOrigin: true,
+      secure: false,
+      rewrite: (path) => path.replace(/^\/api/, ""),
+    },
+  },
+});
