a04c5cfa4ddc83d9c2dc537719126f5f37cd8126
AniketPithadia <aniketpithadia@gmail.com>
2024-01-06 12:50:48 -0500

    Cors error resolved

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
 function App() {
-  axios.defaults.baseURL = "https://simple-chat-app-8qet.vercel.app/";
+  axios.defaults.baseURL = "/api";
   axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
