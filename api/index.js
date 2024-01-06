05cb208d0200b84a831df392d648aa1b544bb87c
AniketPithadia <aniketpithadia@gmail.com>
2024-01-06 13:23:04 -0500

    Cors error resolved

--- api/index.js
+++ api/index.js
@@ -36,7 +36,7 @@ app.use(
 app.use((req, res, next) => {
   res.header(
     "Access-Control-Allow-Origin",
-    "https://your-react-app-domain.com"
+    "https://simple-chat-app-mu.vercel.app/"
   );
   res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
