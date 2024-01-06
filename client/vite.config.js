e21157e6e9a968bdd5a52885bbfa22edb2fa89d5
AniketPithadia <aniketpithadia@gmail.com>
2024-01-06 13:09:05 -0500

    Cors error resolved

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       "/": {
         target: "https://simple-chat-app-8qet.vercel.app/", // Replace with your backend server URL
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\//, ""),
       },
     },
   },
